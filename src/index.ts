37316a6c745d9f1c7c58af41793c5d67c8d7c749
rayangler <27821750+rayangler@users.noreply.github.com>
2023-04-05 15:02:08 -0400

    Create more boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello world');
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.get('/:repo/:branch/documents/:buildId', (req, res) => {
+  const obj = {
+    repo: req.params.repo,
+    branch: req.params.branch,
+    buildId: req.params.buildId,
+  };
+  res.send(obj);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
