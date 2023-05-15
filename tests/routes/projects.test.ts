50ebff9d75363a1da36693f733e5ca7d0666920d
rayangler <27821750+rayangler@users.noreply.github.com>
2023-05-15 15:36:17 -0400

    Create tests

--- tests/routes/projects.test.ts
+++ tests/routes/projects.test.ts
@@ -1,7 +1,8 @@
 import { MongoClient } from 'mongodb';
 import request from 'supertest';
 import { setupApp } from '../../src/app';
-import sampleUpdatedPageDocuments from '../sampleData/updatedDocuments.json';
+import { sampleUpdatedPageDocuments } from '../sampleData/updatedDocuments';
+import sampleMetadataDocuments from '../sampleData/metadata.json';
 
 describe('Test projects routes', () => {
   // process.env.ATLAS_URI should be defined by default in globalSetup.ts
@@ -23,5 +24,21 @@ describe('Test projects routes', () => {
     expect(res.body.data.metadata).toHaveLength(1);
     expect(res.body.data.assets).toHaveLength(3);
     expect(res.body.timestamp).toBeTruthy();
+  });
+
+  it('should return documents updated after given timestamp', async () => {
+    const prevBuildTime = sampleMetadataDocuments[0].created_at['$date'];
+    const timestamp = new Date(prevBuildTime).getTime();
+    const res = await request(app).get(`/projects/docs/master/documents/updated/${timestamp}`);
+    expect(res.status).toBe(200);
+    // Only return documents that have been updated
+    expect(res.body.data.documents).toHaveLength(2);
+    // Only return latest metadata document
+    expect(res.body.data.metadata).toHaveLength(1);
+    // Latest sample metadata document intentionally has the same creation time as the updated time
+    // of the pages we want
+    expect(res.body.data.documents[0].updated_at === res.body.data.metadata[0].created_at);
+    // Only return assets for documents that have been updated
+    expect(res.body.data.assets).toHaveLength(1);
   });
 });
