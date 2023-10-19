1966068c0404804ae9367876bd466b784216a096
rayangler <27821750+rayangler@users.noreply.github.com>
2023-10-19 11:06:03 -0400

    Hotfix: Filter out internal (#32)

--- src/services/pool.ts
+++ src/services/pool.ts
@@ -61,7 +61,7 @@ export const initPoolDb = (client: MongoClient) => {
   return db;
 };
 
-export const findAllRepos = async (options: FindOptions = {}, isProd: boolean, reqId?: string) => {
+export const findAllRepos = async (options: FindOptions = {}, reqId?: string) => {
   try {
     const defaultSort: FindOptions = {
       sort: { repoName: 1 },
@@ -76,11 +76,7 @@ export const findAllRepos = async (options: FindOptions = {}, isProd: boolean, r
       },
     };
     const findOptions = { ...defaultSort, ...options, ...strictOptions };
-    const query: Filter<RepoDocument> = isProd
-      ? {
-          internalOnly: false,
-        }
-      : {};
+    const query: Filter<RepoDocument> = { internalOnly: false };
     return db.collection<RepoDocument>(REPOS_COLLECTION).find(query, findOptions).map(mapRepos).toArray();
   } catch (e) {
     logger.error(createMessage(`Error while finding all repos: ${e}`, reqId));
